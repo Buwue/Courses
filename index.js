793aef883b6307608f02b389e8aeb849066bd7b9
Buwue <88902780+Buwue@users.noreply.github.com>
2021-09-11 14:14:14 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var alphabet = ["Q","W","E","R","T","Y","U","I","O","P","A","S","D","F","G","H","J","K","L","Z","X","C","V","B","N","M","SPACE","COMMA"];
 var numberOfTimes = 0;
+var mouseCPStimes = 0;
+var highestCPS = 0;
 
 
 document.addEventListener("keydown", function(event) {
@@ -23,6 +25,18 @@ document.addEventListener("keyup", function(event) {
     }
     keyRemove(keyPressed);
 });
+
+document.querySelector(".reset-button").addEventListener("click", function(event) {
+    if (event.pointerId === 1) {
+        resetClicks();
+    }
+});
+
+document.querySelector(".mouse-clicker").addEventListener("click", function(event) {
+    if (event.pointerId === 1) {
+        mouseCPStimes++;
+    }
+})
 
 
 function keyHeld(keyPressed) {
@@ -70,7 +84,38 @@ setInterval(function() {
         var spacePresses1Sec = numberOfTimes;
         var spacePresses = spacePresses1Sec - spacePresses0sec;
         var CPS = spacePresses / 1;
+        if (CPS >= 0) {
         document.querySelector(".CPM").innerHTML = "Your CPS is : " + CPS;
         spacePresses0sec = numberOfTimes;
+        } else {
+            document.querySelector(".CPM").innerHTML = "Your CPS is : 0";
+        }
     }, 1000);
 
+function resetClicks() {
+    var elementsWithKeyPressed = document.querySelectorAll(".key-pressed");
+    
+    for (var i = 0; i < elementsWithKeyPressed.length; i++) {
+        document.querySelector(".key-pressed").classList.remove("key-pressed");
+        
+    }
+
+    numberOfTimes = 0;
+
+    document.querySelector(".letter-space").textContent = "Press me!"; 
+
+}
+
+mouseCPStimes0sec = mouseCPStimes;
+setInterval(function() {
+    mouseCPStimes1sec = mouseCPStimes;
+    var mouseCPS = mouseCPStimes1sec - mouseCPStimes0sec;
+    document.querySelector(".mouse-CPS").innerHTML = ("Your CPS is : " + mouseCPS);
+    mouseCPStimes0sec = mouseCPStimes;
+    console.log("hi");
+
+    if (mouseCPS > highestCPS) {
+        highestCPS = mouseCPS;
+        document.querySelector(".highest-mouse-CPS").innerHTML = ("Your highest CPS is : " + highestCPS);
+    }
+}, 1000);
